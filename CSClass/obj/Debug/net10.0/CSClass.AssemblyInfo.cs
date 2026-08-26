76b654344352f586c91bfe82ac596b2fa17d8004
Vibhavari Borole <vab030201@gmail.com>
2026-08-26 12:12:30 +0530

    Updated Serialization code

--- CSClass/obj/Debug/net10.0/CSClass.AssemblyInfo.cs
+++ CSClass/obj/Debug/net10.0/CSClass.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSClass")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e730833aa4c58e4235de2892395314e517213688")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e182d13087744f97556ab789967d76e8969fdeec")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSClass")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSClass")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Methods/obj/Debug/net10.0/Methods.AssemblyInfo.cs
+++ Methods/obj/Debug/net10.0/Methods.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Methods")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdd2e22ab09b53b8887bc94bf22a6969a69399ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e182d13087744f97556ab789967d76e8969fdeec")]
 [assembly: System.Reflection.AssemblyProductAttribute("Methods")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Methods")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
