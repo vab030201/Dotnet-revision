e5bbb7f92eb125e3821be4f3657f363f8e2913e0
Vibhavari Borole <vab030201@gmail.com>
2026-08-31 09:10:59 +0530

    file

---
+++ Fullprgm/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace TFLDemo;
+
+    public class Student
+{
+    public string Name {get; set;}
+    public int Age {get; set;}
+
+    public Student (string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine($"Name: {Name}");
+        Console.WriteLine($"Age: {Age}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student student = new  Student("Durga", 29);
+
+
+        student.Display();
+    }
+}
+
+
+
+
