d2c8f55591c81a64e61a22bd58c498bdf2ce041b
Vibhavari Borole <vab030201@gmail.com>
2026-09-01 01:19:30 +0530

    examplesoop

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -43,3 +43,105 @@ namespace OOP
         }
     }
 }
+
+
+// Encapsulation 
+class BankAccount
+{
+    public decimal Balance { get; private set; }
+
+    public void Deposit(decimal amount)
+    {
+        if (amount > 0)
+        {
+            Balance += amount;
+        }
+    }
+}
+
+/* Inheritance
+class Account
+{
+    public decimal Balance { get; set; }
+
+    public void Deposit(decimal amount)
+    {
+        Balance += amount;
+    }
+
+    public void Withdraw(decimal amount)
+    {
+        if (Balance >= amount)
+        {
+            Balance -= amount;
+        }
+    }
+}
+
+class SavingAccount : Account
+{
+    public decimal InterestRate { get; set; } = 0.05m;
+
+    public void ApplyInterest()
+    {
+        Balance += Balance * InterestRate;
+    }
+}
+
+class CurrentAccount : Account
+{
+    public decimal OverdraftLimit { get; set; }
+}
+
+//polymorphism
+
+class Account
+{
+    public decimal Balance { get; set; }
+
+    public virtual void ProcessTransaction(decimal amount)
+    {
+        Balance += amount;
+
+        Console.WriteLine(
+            $"Generic Account: {Balance}"
+        );
+    }
+}
+Now SavingAccount:
+
+class SavingAccount : Account
+{
+    public decimal InterestRate { get; set; } = 0.05m;
+
+    public override void ProcessTransaction(decimal amount)
+    {
+        Balance += amount;
+
+        Balance += Balance * InterestRate;
+
+        Console.WriteLine(
+            $"Saving Account: {Balance}"
+        );
+    }
+}
+And CurrentAccount:
+
+class CurrentAccount : Account
+{
+    public override void ProcessTransaction(decimal amount)
+    {
+        Balance += amount;
+
+        Console.WriteLine(
+            $"Current Account: {Balance}"
+        );
+    }
+}
+Now:
+
+Account acc1 = new SavingAccount();
+Account acc2 = new CurrentAccount();
+
+acc1.ProcessTransaction(1000);
+acc2.ProcessTransaction(1000);
