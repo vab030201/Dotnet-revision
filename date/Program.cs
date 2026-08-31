fe1071a8e393d3672c5070a91efa475aac3478bf
Vibhavari Borole <vab030201@gmail.com>
2026-08-31 14:12:37 +0530

    demo

---
+++ date/Program.cs
@@ -0,0 +1,28 @@
+﻿public class Date
+{
+    private int day;
+    private int month;
+    private int year;
+
+    public Date(int d, int m, int y)
+    {
+        this.day = d;
+        this.month = m;
+        this.year = y;
+    }
+
+    public void Show()
+    {
+        Console.WriteLine(
+            this.day + "/" +
+            this.month + "/" +
+            this.year);
+    }
+
+    public static void Main(string[] args)
+    {
+        Date today = new Date(12, 10, 2025);
+
+        today.Show();
+    }
+}
